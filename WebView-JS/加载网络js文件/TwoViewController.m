22012c0f8541674c623d20903ee415b249e6d1ec
wushangkun <wushangkun@j1.com>
2016-04-20 10:54:49 +0800

    加载本地的js,html,css

---
+++ WebView-JS/加载网络js文件/TwoViewController.m
@@ -0,0 +1,23 @@
+//
+//  TwoViewController.m
+//  WebView-JS
+//
+//  Created by wushangkun on 16/4/19.
+//  Copyright © 2016年 wushangkun. All rights reserved.
+//
+
+#import "TwoViewController.h"
+
+@interface TwoViewController ()
+
+@end
+
+@implementation TwoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+
+@end
